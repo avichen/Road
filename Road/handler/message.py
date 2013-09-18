d03ec052c06536891fe431f8fab02bbb94eb1845
eric.chen <eric.chen.china@gmail.com>
2013-09-18 16:03:53 +0800

    init message list

--- Road/handler/message.py
+++ Road/handler/message.py
@@ -9,7 +9,7 @@ from Base import *
 import time
 from json import _default_encoder
 
-class messageUserUnreadHandler(BaseHandler):
+class MessageUserUnreadHandler(BaseHandler):
     def get(self):
         user_info = self.get_current_user()
         id = 41
@@ -20,17 +20,23 @@ class messageUserUnreadHandler(BaseHandler):
             self.write(tornado.escape.json_encode(message_info))
         return None
     
-class messageUserUnreadCountHandler(BaseHandler):
+class MessageUserUnreadCountHandler(BaseHandler):
     def get(self):
         user_info = self.get_current_user()
-        id = 41
-        strSql = "select count(*) count from message_info where rcv_id = "+bytes(id)+" and TO_DAYS(now()) - TO_DAYS(post_time) <= 30 and status = 0"
+        strSql = "select count(*) count from message_info where rcv_id = "+user_info["id"]+" and TO_DAYS(now()) - TO_DAYS(post_time) <= 30 and status = 0"
         count = self.db.query(strSql)
-        LOGGER.debug(count)
+#         LOGGER.debug(count)
         if count:
             self.write(tornado.escape.json_encode(count))
         return None
 
+class MessageListHandler(BaseHandler):
+    def get(self):
+        menu_code = self.get_argument("m")
+        user_info = self.get_current_user()
+        strSql = "select * from message_info where rcv_id = %s" % user_info["id"]
+        message_list = self.db.query(strSql)
 
+        self.render("message/messagelist.html", message_list = message_list, menu_code = menu_code)
         
         
